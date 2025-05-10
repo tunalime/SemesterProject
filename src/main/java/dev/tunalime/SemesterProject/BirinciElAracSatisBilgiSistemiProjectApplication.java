55ed9430c74bcefae34d55503619b4393e9c88b5
Mehmet Fethi Tunalı <103124921+tunalime@users.noreply.github.com>
2025-05-10 21:13:52 +0300

    Add files via upload

---
+++ src/main/java/dev/tunalime/SemesterProject/BirinciElAracSatisBilgiSistemiProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.tunalime.SemesterProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirinciElAracSatisBilgiSistemiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirinciElAracSatisBilgiSistemiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tunalime/SemesterProject/BirinciElAracSatisBilgiSistemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tunalime.SemesterProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirinciElAracSatisBilgiSistemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
